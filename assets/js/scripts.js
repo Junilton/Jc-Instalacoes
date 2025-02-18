bf9f3b3212014f9c6971e68261334e88ff0a35bc
Junilton <Junilton.dacosta@hotmail.com>
2025-02-18 11:00:24 -0300

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+// Exemplo de script para menu mobile (caso seja necessário futuramente)
+document.addEventListener('DOMContentLoaded', function() {
+  // Aqui você pode implementar funcionalidades adicionais, como um menu responsivo
+  console.log('Scripts carregados com sucesso!');
+});
